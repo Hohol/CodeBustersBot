5f9b43b1adc65d9927422bc6f191dca485783f7d
Hohol <nikita.glashenko@gmail.com>
2016-06-26 03:15:45 +0500

    Initial commit

---
+++ src/game/Interactor.java
@@ -0,0 +1,32 @@
+package game;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class Interactor {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+        int bustersPerPlayer = in.nextInt(); // the amount of busters you control
+        int ghostCount = in.nextInt(); // the amount of ghosts on the map
+        int myTeamId = in.nextInt(); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
+
+        // game loop
+        while (true) {
+            int entities = in.nextInt(); // the number of busters and ghosts visible to you
+            for (int i = 0; i < entities; i++) {
+                int entityId = in.nextInt(); // buster id or ghost id
+                int x = in.nextInt();
+                int y = in.nextInt(); // position of this buster / ghost
+                int entityType = in.nextInt(); // the team id if it is a buster, -1 if it is a ghost.
+                int state = in.nextInt(); // For busters: 0=idle, 1=carrying a ghost.
+                int value = in.nextInt(); // For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+            }
+            for (int i = 0; i < bustersPerPlayer; i++) {
+
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+
+                System.out.println("MOVE 8000 4500"); // MOVE x y | BUST id | RELEASE
+            }
+        }
+    }
+}
